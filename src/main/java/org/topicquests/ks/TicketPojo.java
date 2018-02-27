6c3cbe350e59d167b8bd0d8cac8f5b71e9c92f42
John Stewart <jstewart148@gmail.com>
2018-02-27 13:01:31 -0500

    update Ticket to include fields found in the user database schema

--- src/main/java/org/topicquests/ks/TicketPojo.java
+++ src/main/java/org/topicquests/ks/TicketPojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015, TopicQuests
+ * Copyright 2018, TopicQuests
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,131 +20,210 @@ import java.util.*;
 import org.topicquests.ks.api.ITQCoreOntology;
 import org.topicquests.ks.api.ITicket;
 
-
-
-/**
- * @author park
- *
- */
 public class TicketPojo implements ITicket {
-	private Map<String,Object>data;
-	private final String
-		MEMBER_KEY		= "memberlist",
-		AVATAR_KEY		= "avatarlist",
-		LOCATOR_KEY		= "locator";
+  private Map<String,Object>data;
+  private final String
+  MEMBER_KEY		= "memberlist",
+    AVATAR_KEY		= "avatarlist",
+    LOCATOR_KEY		= "locator",
+    HANDLE_KEY		= "handle",
+    EMAIL_KEY           = "email",
+    LANGUAGE_KEY        = "language",
+    FIRST_NAME_KEY      = "firstName",
+    LAST_NAME_KEY       = "lastName",
+    ACTIVE_KEY          = "active";
 
-	/**
-	 * 
-	 */
-	public TicketPojo() {
-		data = new HashMap<String,Object>();
-		
-	}
+  public TicketPojo() {
+    data = new HashMap<String,Object>();
+  }
 	
-	public TicketPojo(String userLocator) {
-		data = new HashMap<String,Object>();
-		data.put(LOCATOR_KEY, userLocator);
-	}
+  public TicketPojo(String userLocator) {
+    data = new HashMap<String,Object>();
+    setUserLocator(userLocator);
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#setUserLocator(java.lang.String)
-	 */
-	@Override
-	public void setUserLocator(String locator) {
-		data.put(LOCATOR_KEY, locator);
-	}
+  public TicketPojo(String userLocator, String email, String password,
+                    String handle) {
+    this(userLocator, email, password, handle, null, null);
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#getUserLocator()
-	 */
-	@Override
-	public String getUserLocator() {
-		return (String)data.get(LOCATOR_KEY);
-	}
+  public TicketPojo(String userLocator, String email, String password,
+                    String handle, String first_name, String last_name) {
+    this(userLocator, email, password, handle, first_name, last_name, "en");
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#addAvatarLocator(java.lang.String)
-	 */
-	@Override
-	public void addAvatarLocator(String avatar) {
-		List<String>l = (List<String>)data.get(AVATAR_KEY);
-		if (l == null) {
-			l = new ArrayList<String>();
-			data.put(AVATAR_KEY, l);
-		}
-		if (!l.contains(avatar))
-			l.add(avatar);
+  public TicketPojo(String userLocator, String email, String password,
+                    String handle, String first_name, String last_name,
+                    String language) {
+    data = new HashMap<String,Object>();
+    setUserLocator(userLocator);
+    setEmail(email);
+    setHandle(handle);
+    setFirstName(first_name);
+    setLastName(last_name);
+    setLanguage(language);
+    setActive(true);
+  }
 
-	}
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#setUserLocator(java.lang.String)
+   */
+  @Override
+  public void setUserLocator(String locator) {
+    data.put(LOCATOR_KEY, locator);
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#listAvatars()
-	 */
-	@Override
-	public List<String> listAvatars() {
-		List<String>l = (List<String>)data.get(AVATAR_KEY);
-		if (l == null)
-			l = new ArrayList<String>();
-		return l;
-	}
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#getUserLocator()
+   */
+  @Override
+  public String getUserLocator() {
+    return (String)data.get(LOCATOR_KEY);
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#addGroupLocator(java.lang.String)
-	 */
-	@Override
-	public void addGroupLocator(String groupLocator) {
-		List<String>l = (List<String>)data.get(MEMBER_KEY);
-		if (l == null) {
-			l = new ArrayList<String>();
-			data.put(MEMBER_KEY, l);
-		}
-		if (!l.contains(groupLocator))
-			l.add(groupLocator);
-	}
+  @Override
+  public void setEmail(String email) {
+    data.put(EMAIL_KEY, email);
+  }
+  
+  @Override
+  public String getEmail() {
+    return (String)data.get(EMAIL_KEY);
+  }
+  
+  @Override
+  public void setHandle(String handle) {
+    data.put(HANDLE_KEY, handle);
+  }
+  
+  @Override
+  public String getHandle() {
+    return (String)data.get(HANDLE_KEY);
+  }
+  
+  @Override
+  public void setFirstName(String firstName) {
+    data.put(FIRST_NAME_KEY, firstName);
+  }
+  
+  @Override
+  public String getFirstName() {
+    return (String)data.get(FIRST_NAME_KEY);
+  }
+  
+  @Override
+  public void setLastName(String lastName) {
+    data.put(LAST_NAME_KEY, lastName);
+  }
+  
+  @Override
+  public String getLastName() {
+    return (String)data.get(LAST_NAME_KEY);
+  }
+  
+  @Override
+  public void setLanguage(String language) {
+    data.put(LANGUAGE_KEY, language);
+  }
+  
+  @Override
+  public String getLanguage() {
+    return (String)data.get(LANGUAGE_KEY);
+  }
+  
+  @Override
+  public void setActive(boolean active) {
+    data.put(ACTIVE_KEY, new Boolean(active));
+  }
+  
+  @Override
+  public boolean getActive() {
+    return ((Boolean)data.get(ACTIVE_KEY)).booleanValue();
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#listGroupLocators()
-	 */
-	@Override
-	public List<String> listGroupLocators() {
-		List<String>l = (List<String>)data.get(MEMBER_KEY);
-		if (l == null)
-			l = new ArrayList<String>();
-		return l;
-	}
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#addAvatarLocator(java.lang.String)
+   */
+  @Override
+  public void addAvatarLocator(String avatar) {
+    List<String>l = (List<String>)data.get(AVATAR_KEY);
+    if (l == null) {
+      l = new ArrayList<String>();
+      data.put(AVATAR_KEY, l);
+    }
+    if (!l.contains(avatar))
+      l.add(avatar);
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#isAuthenticated()
-	 */
-	@Override
-	public boolean isAuthenticated() {
-		String lox = getUserLocator();
-		return !lox.equals(ITQCoreOntology.GUEST_USER);
-	}
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#isMember(java.lang.String)
-	 */
-	@Override
-	public boolean isMember(String groupLocator) {
-		List<String>l = listGroupLocators();
-		return l.contains(groupLocator);
-	}
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#listAvatars()
+   */
+  @Override
+  public List<String> listAvatars() {
+    List<String>l = (List<String>)data.get(AVATAR_KEY);
+    if (l == null)
+      l = new ArrayList<String>();
+    return l;
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#setProperty(java.lang.String)
-	 */
-	@Override
-	public void setProperty(String key, Object value) {
-		data.put(key, value);
-	}
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#addGroupLocator(java.lang.String)
+   */
+  @Override
+  public void addGroupLocator(String groupLocator) {
+    List<String>l = (List<String>)data.get(MEMBER_KEY);
+    if (l == null) {
+      l = new ArrayList<String>();
+      data.put(MEMBER_KEY, l);
+    }
+    if (!l.contains(groupLocator))
+      l.add(groupLocator);
+  }
 
-	/* (non-Javadoc)
-	 * @see org.topicquests.ks.model.api.ITicket#getProperty(java.lang.String)
-	 */
-	@Override
-	public Object getProperty(String key) {
-		return data.get(key);
-	}
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#listGroupLocators()
+   */
+  @Override
+  public List<String> listGroupLocators() {
+    List<String>l = (List<String>)data.get(MEMBER_KEY);
+    if (l == null)
+      l = new ArrayList<String>();
+    return l;
+  }
+
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#isAuthenticated()
+   */
+  @Override
+  public boolean isAuthenticated() {
+    String lox = getUserLocator();
+    return !lox.equals(ITQCoreOntology.GUEST_USER);
+  }
+
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#isMember(java.lang.String)
+   */
+  @Override
+  public boolean isMember(String groupLocator) {
+    List<String>l = listGroupLocators();
+    return l.contains(groupLocator);
+  }
+
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#setProperty(java.lang.String)
+   */
+  @Override
+  public void setProperty(String key, Object value) {
+    data.put(key, value);
+  }
+
+  /* (non-Javadoc)
+   * @see org.topicquests.model.api.ITicket#getProperty(java.lang.String)
+   */
+  @Override
+  public Object getProperty(String key) {
+    return data.get(key);
+  }
 
 }
--- src/main/java/org/topicquests/ks/api/ITicket.java
+++ src/main/java/org/topicquests/ks/api/ITicket.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015, TopicQuests
+ * Copyright 2018, TopicQuests
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,67 +17,110 @@ package org.topicquests.ks.api;
 
 import java.util.List;
 
-/**
- * @author park
- *
- */
 public interface ITicket {
 	
-	/**
-	 * General purpose expansion capabilities
-	 * @param key
-	 * @param value
-	 */
-	void setProperty(String key, Object value);
+  /**
+   * General purpose expansion capabilities
+   * @param key
+   * @param value
+   */
+  void setProperty(String key, Object value);
+  Object getProperty(String key);
 	
-	Object getProperty(String key);
-	
-	/**
-	 * <code>locator</code> is the <em>primary</em> identifier
-	 * for this user
-	 * @param locator
-	 */
-	void setUserLocator(String locator);
-	String getUserLocator();
+  /**
+   * <code>locator</code> is the <em>primary</em> identifier
+   * for this user
+   * @param locator
+   */
+  void setUserLocator(String locator);
+  String getUserLocator();
 
-	/**
-	 * A user can have one or more, depending on system rules,
-	 * <code>avatar</code> identities
-	 * @param avatar
-	 */
-	void addAvatarLocator(String avatar);
+  /**
+   * <code>email</code> is the email address associated
+   * with this user
+   * @param email
+   */
+  void setEmail(String email);
+  String getEmail();
+  
+  /**
+   * <code>handle</code> is the handle associated
+   * with this user
+   * @param handle
+   */
+  void setHandle(String handle);
+  String getHandle();
+  
+  /**
+   * <code>firstName</code> is the first name of the
+   * registered user
+   * @param locator
+   */
+  void setFirstName(String firstName);
+  String getFirstName();
+  
+  /**
+   * <code>lastName</code> is the last name of the 
+   * registered user
+   * @param lastName
+   */
+  void setLastName(String lastName);
+  String getLastName();
+  
+  /**
+   * <code>language</code> is the primary language used
+   * by the  user
+   * @param language
+   */
+  void setLanguage(String language);
+  String getLanguage();
+  
+  /**
+   * <code>active</code> denotes whether the user is
+   * active in the system
+   * @param active
+   */
+  void setActive(boolean active);
+  boolean getActive();
+  
+  /**
+   * A user can have one or more, depending on system rules,
+   * <code>avatar</code> identities
+   * @param avatar
+   */
+  void addAvatarLocator(String avatar);
 	
-	/**
-	 * Return list of avatar identities
-	 * @return does not return <code>null</code>
-	 */
-	List<String> listAvatars();
+  /**
+   * Return list of avatar identities
+   * @return does not return <code>null</code>
+   */
+  List<String> listAvatars();
 	
-	/**
-	 * Some groups or documents may have ACLs which grant
-	 * a group identity
-	 * @param groupLocator
-	 */
-	void addGroupLocator(String groupLocator);
+  /**
+   * Some groups or documents may have ACLs which grant
+   * a group identity
+   * @param groupLocator
+   */
+  void addGroupLocator(String groupLocator);
 	
-	/**
-	 * Return a list of group locators
-	 * @return does not return <code>null</code>
-	 */
-	List<String> listGroupLocators();
+  /**
+   * Return a list of group locators
+   * @return does not return <code>null</code>
+   */
+  List<String> listGroupLocators();
 	
-	/**
-	 * Will return <code>true</code> if <code>userlocator</code> is
-	 * not equal to {@link ITopicQuestsOntology&GUEST_USER}
-	 * @return
-	 */
-	boolean isAuthenticated();
+  /**
+   * Will return <code>true</code> if <code>userlocator</code> is
+   * not equal to {@link ITopicQuestsOntology&GUEST_USER}
+   * @return
+   */
+  boolean isAuthenticated();
 	
-	/**
-	 * Will return <code>true</code> if this user is a has
-	 * <code>groupLocator</code> in the member list
-	 * @param groupLocator
-	 * @return
-	 */
-	boolean isMember(String groupLocator);
+  /**
+   * Will return <code>true</code> if this user is a has
+   * <code>groupLocator</code> in the member list
+   * @param groupLocator
+   * @return
+   */
+  boolean isMember(String groupLocator);
 }
