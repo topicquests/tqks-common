102c379b001ec34f10690370a7745205882f0b62
John Stewart <jstewart148@gmail.com>
2018-03-27 08:43:35 -0400

    delete data from the superclasses and transitive_closure tables before deleting from the proxy table in cleandb.sql

--- config/cleandb.sql
+++ config/cleandb.sql
@@ -5,6 +5,8 @@
 SET ROLE tq_admin;
 
 DELETE FROM tq_contents.properties;
+DELETE FROM tq_contents.superclasses;
+DELETE FROM tq_contents.transitive_closure;
 DELETE FROM tq_contents.proxy;
 
 SET ROLE tq_users;
