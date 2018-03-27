fc9394968fb20fc2afb90c8b7082877bb512c9b9
John Stewart <jstewart148@gmail.com>
2018-03-27 12:44:32 -0400

    clean the acls table in cleandb.sql

--- config/cleandb.sql
+++ config/cleandb.sql
@@ -7,6 +7,7 @@ SET ROLE tq_admin;
 DELETE FROM tq_contents.properties;
 DELETE FROM tq_contents.superclasses;
 DELETE FROM tq_contents.transitive_closure;
+DELETE FROM tq_contents.acls;
 DELETE FROM tq_contents.proxy;
 
 SET ROLE tq_users;
