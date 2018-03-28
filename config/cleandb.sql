d6a2e31756e5ada19694a39e094736d7dc7faefa
John Stewart <jstewart148@gmail.com>
2018-03-28 11:26:56 -0400

    delete logged actions from the audit table in cleandb.sql

--- config/cleandb.sql
+++ config/cleandb.sql
@@ -8,11 +8,13 @@ DELETE FROM tq_contents.properties;
 DELETE FROM tq_contents.superclasses;
 DELETE FROM tq_contents.transitive_closure;
 DELETE FROM tq_contents.acls;
+DELETE FROM tq_contents.psi;
 DELETE FROM tq_contents.proxy;
 
 SET ROLE tq_users;
 
 DELETE FROM tq_authentication.users;
+DELETE FROM audit.logged_actions;
 
 \q
 
