383cedcbb01f0e5f19e979cbcb7ee4c40210c036
Jack Park <jackpark@gmail.com>
2018-06-14 15:42:50 -0700

    Added kafka keys

---
+++ src/main/java/org/topicquests/ks/api/ICoreKafkaKeys.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.topicquests.ks.api;
+
+/**
+ * @author jackpark
+ *
+ */
+public interface ICoreKafkaKeys {
+	public static final String
+		KAFKA_VERB		= "type",
+		KAFKA_CARGO		= "cargo";
+}
